d064ce79b463e19e95a923df334056d5b9e86ad4
CrownZone <ayazsirmc@gmail.com>
2026-07-13 06:12:25 +0300

    Replace ZombieRenderer with WatcherRenderer for WATCHER

--- src/main/java/com/example/verity/client/VerityModClient.java
+++ src/main/java/com/example/verity/client/VerityModClient.java
@@ -3,13 +3,12 @@ package com.example.verity.client;
 import com.example.verity.entity.ModEntities;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.renderer.entity.ZombieRenderer;
 
 public class VerityModClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(ModEntities.WATCHER, ZombieRenderer::new);
+        EntityRendererRegistry.register(ModEntities.WATCHER, WatcherRenderer::new);
         JumpscareManager.register();
     }
 }
